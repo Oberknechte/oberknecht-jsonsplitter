9bb451b5f59cf43533277b7bb80fc6044ba1a155
jubewe <ju.be.we@posteo.de>
2023-03-02 20:33:26 +0100

    v0.1.5

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 require("./lib/arguments/jsonsplitter.options");
 
-
-const jsonsplitter = require("./lib/splitter/jsonsplitter");
-
-module.exports = {
-    jsonsplitter: jsonsplitter
+module.exports = class {
+    static jsonsplitter = require("./lib/splitter/jsonsplitter");
 };
